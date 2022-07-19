cfd53e559796a7e70ceff28422c0acdbe3ca9b69
Joshua Aarón Díaz Robayna <jadiaz@hiberus.com>
2022-07-19 09:44:34 +0200

    Terminada la  seccion de introducción

---
+++ 02-bases/src/app/dbz/agregar/agregar.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html',
+})
+export class AgregarComponent {
+
+  @Input() nuevo: Personaje = {
+    nombre: '',
+    poder: 0,
+  };
+
+  constructor (private DbzService: DbzService) {}
+
+  // @Output() OnNewPersonaje: EventEmitter<Personaje> = new EventEmitter();
+
+  agregar() {
+    if (this.nuevo.nombre.trim().length === 0) {
+      return;
+    }
+
+    this.DbzService.agregarPersonaje(this.nuevo)
+
+    // this.OnNewPersonaje.emit(this.nuevo)
+
+    this.nuevo = {
+      nombre: '',
+      poder: 0,
+    };
+  }
+}
